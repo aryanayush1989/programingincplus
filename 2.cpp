b60f67da40784d9ca529ffd000d9b19979ea9bee
aryanayush1989 <145833299+aryanayush1989@users.noreply.github.com>
2023-09-26 20:05:30 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a = 8, b = 3;
+
+	// Addition 
+	cout << "a + b = " << (a + b) << endl;
+
+	// Subtraction 
+	cout << "a - b = " << (a - b) << endl;
+
+	// Multiplication 
+	cout << "a * b = " << (a * b) << endl;
+
+	// Division 
+	cout << "a / b = " << (a / b) << endl;
+
+	// Modulo 
+	cout << "a % b = " << (a % b) << endl;
+
+	return 0;
+}
